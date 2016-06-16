b2ba2f3072f6292b2c97ec6292706658dcf8681e
Garrick Sherman <backwardstg@gmail.com>
2016-06-16 00:51:04 -0700

    added on delete cascade

--- src/models.py
+++ src/models.py
@@ -3,10 +3,10 @@ from datetime import datetime
 from peewee import *
 
 
-db_path=os.path.abspath(os.path.join(os.path.dirname(__file__),
+db_path = os.path.abspath(os.path.join(os.path.dirname(__file__),
 	os.path.pardir,
 	'lists.db'))
-db=SqliteDatabase(db_path)
+db = SqliteDatabase(db_path, pragmas=[('foreign_keys', 'ON')])
 
 class BaseModel(Model):
 	created=DateTimeField(default=datetime.now)
@@ -20,13 +20,13 @@ class User(BaseModel):
 	password=CharField()
 
 class List(BaseModel):
-	owner=ForeignKeyField(User, related_name='lists')
+	owner=ForeignKeyField(User, related_name='lists', on_delete='CASCADE')
 	title=CharField()
 	description=CharField(null=True)
 	public=IntegerField(default=0) # 0 is private, 1 is public
 
 class Item(BaseModel):
-	collection=ForeignKeyField(List, related_name='items')
+	collection=ForeignKeyField(List, related_name='items', on_delete='CASCADE')
 	title=CharField()
 	description=CharField(null=True)
 	number=IntegerField(default=-1) # -1 is no pref, comes after ordered items
